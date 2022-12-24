5717895e6584839f94102f5818749ab92c74456b
M1K259 <87774553+Mika259@users.noreply.github.com>
2022-12-24 19:39:09 +0800

    Create termux_setup.sh

---
+++ termux_setup.sh
@@ -0,0 +1,8 @@
+pkg install g++ -y
+pkg install gcc -y
+pkg install toilet -y
+echo ""
+echo "If Setup Failed Go in http://github.com/Mika259/launch_gc/help.txt"
+echo ""
+sleep 2
+rm termux_setup.sh
